f23ee6296a7ac09063361c037da52ffa035d7c75
Ramiro <ramegp@gmail.com>
2021-11-15 20:06:32 -0300

    Subido desde node

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ const app = express();
 app.get('/',(req:express.Request, res:express.Response)=>{
     res.json({msj:"Hola"})
 })
-
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
     console.log("escuchando puerto 5000");
     
 })
